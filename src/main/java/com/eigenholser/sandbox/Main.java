95f62802f5289ee9f78d506aff3fb2a611fb8572
Scott Overholser <eigenholser@users.noreply.github.com>
2021-04-06 20:33:03 -0600

    Convert to Multi

--- src/main/java/com/eigenholser/sandbox/Main.java
+++ src/main/java/com/eigenholser/sandbox/Main.java
@@ -1,7 +1,7 @@
 package com.eigenholser.sandbox;
 
 import com.eigenholser.sandbox.model.Beer;
-import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.Multi;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
@@ -9,7 +9,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/")
 public class Main {
@@ -20,8 +20,14 @@ public class Main {
     @GET
     @Path("/punk")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<List<Beer>> getBeers() {
+    public Multi<Beer> getBeers() {
         System.out.println("**** Getting Beers ****");
-        return punk.get(1);
+        return Multi.createBy().repeating()
+                .uni(() -> new AtomicInteger(), p -> {
+                    System.out.println("Page: " + p);
+                    return punk.get(p.incrementAndGet());
+                })
+                .until(l -> l.isEmpty())
+        .onItem().disjoint();
     }
 }
